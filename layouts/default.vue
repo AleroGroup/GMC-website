79a17f24884017bd2c5406987ede5521f43381e8
Stephanie Kirathe <stephaniekirathe@gmail.com>
2019-07-10 05:12:54 +0300

    Setting up

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <!-- <v-toolbar
+      fixed
+      app
+    >
+    </v-toolbar> -->
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app >
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-layout>
+    <h2>hello</h2>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'GreatMinds Challenge | Home',
+      meta: [
+        { hid: 'description', name: 'description', content: 'Great Minds Challenge' },
+        { hid: 'description', name: 'description', content: 'GMC' }
+      ]
+
+    }
+  }
+}
+</script>
