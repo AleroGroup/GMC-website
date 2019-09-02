08492c750cddb5af873cbdd5fbb10293d01e30f6
Stephanie Kirathe <stephaniekirathe@gmail.com>
2019-09-03 01:42:54 +0300

    .

--- server/server.js
+++ server/server.js
@@ -23,9 +23,8 @@ mongoose.Promise = require('bluebird')
 // let mongo = process.env.MONGODB_PASS
 const methodOverride = require('method-override')
 
-
-const mongoURI = 'mongodb://alero:oErOZoLW1@ds215988.mlab.com:15988/heroku_jb1kdl7j'
-/* 'mongodb+srv://alero:oErOZoLW@cluster0-jsnt7.mongodb.net/userForms?retryWrites=true&w=majority' */
+//'mongodb://alero:oErOZoLW1@ds215988.mlab.com:15988/heroku_jb1kdl7j'
+const mongoURI ='mongodb+srv://alero:oErOZoLW@cluster0-jsnt7.mongodb.net/userForms?retryWrites=true&w=majority'
 mongoose.connect(mongoURI, {
   useNewUrlParser: true
 })
