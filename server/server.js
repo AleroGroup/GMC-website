16e9fdac5e981fc25dd56070bdaeb9e9c9bb6b4f
Stephanie Kirathe <stephaniekirathe@gmail.com>
2019-09-04 15:04:08 +0300

    changes

--- server/server.js
+++ server/server.js
@@ -128,7 +128,7 @@ app.post("/sendmail", multipartMiddleware, function (req, res) {
 
 
 
-/* async function start() {
+ async function start() {
   //Middlewares
   app.use(cors())
   app.use(morgan('dev'))
@@ -199,63 +199,5 @@ app.post("/sendmail", multipartMiddleware, function (req, res) {
   })
 }
 
-start() */
+start()
 
-
-//Middlewares
-app.use(cors())
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(methodOverride('_method'));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static(path.join(__dirname, 'public')))
-app.use('/uploads', express.static('uploads'));
-
-//app.use('/uploads', express.static('uploads'));
-app.use('/company', companyRouter)
-app.use('/wildcard', wildcardRouter)
-
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', '*')
-  if (req.method === 'OPTIONS') {
-    res.header(
-      'Access-Control-Allow-Methods',
-      'PUT',
-      'POST',
-      'PATCH',
-      'DELETE',
-      'GET'
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
-
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500)
-  res.json({
-    error: {
-      message: error.message
-    }
-  })
-})
-
-const host = process.env.HOST || '127.0.0.1'
-const port = process.env.PORT || 3000
-
-const nuxt = new Nuxt(config)
-
-if (config.dev) {
-  const builder = new Builder(nuxt)
-  builder.build()
-}
-
-app.use(nuxt.render)
-
-app.listen(port, host)
-console.log('Server is listening on ' + host + ':' + port)
